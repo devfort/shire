c2bb6b7d4b222e9dba4f2a3e409df16579387386
Steve Marshall <steve@nascentguruism.com>
2013-03-07 18:06:35 +0000

    Revert and move postgres setup to hobbit.

--- chef/cookbooks/hobbit/recipes/default.rb
+++ chef/cookbooks/hobbit/recipes/default.rb
@@ -5,24 +5,3 @@ end
 execute "install listen gem" do
   command "gem install #{Chef::Config[:file_cache_path]}/listen-0.7.3.gem"
 end
-
-# TODO: Use the real postgresql providers?
-# include_recipe "database::postgresql"
-execute "Install hobbit user" do
-  command "createuser --superuser #{node[:runas]}"
-  user "postgres"
-  not_if "psql -U #{node[:runas]} -l"
-end
-# postgresql_database_user 'hobbit' do
-#   username node[:runas]
-#   superuser true
-# end
-execute "Create hobbit DB" do
-  command "createdb -E UTF8 -T template0 hobbit"
-  user "postgres"
-  not_if "psql -U #{node[:runas]} hobbit -c 'SELECT 1;'"
-end
-# postgresql_database 'hobbit' do
-#   encoding 'UTF-8'
-#   owner node[:runas]
-# end
