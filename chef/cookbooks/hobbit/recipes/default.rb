eed350df268d5048e1f322abc6f44fd148223a11
Steve Marshall <steve@nascentguruism.com>
2013-03-07 16:33:01 +0000

    Remove now-unneeded listen gem. Yay.

--- chef/cookbooks/hobbit/recipes/default.rb
+++ chef/cookbooks/hobbit/recipes/default.rb
@@ -1,10 +1,4 @@
 package "ruby-sass"
-cookbook_file "#{Chef::Config[:file_cache_path]}/listen-0.7.3.gem" do
-  source "listen-0.7.3.gem"
-end
-execute "install listen gem" do
-  command "gem install #{Chef::Config[:file_cache_path]}/listen-0.7.3.gem"
-end
 
 # TODO: Use the real postgresql providers?
 # include_recipe "database::postgresql"
