3fedada764049e3b025337910c8080e083cf5247
Steve Marshall <steve@nascentguruism.com>
2013-03-07 18:03:51 +0000

    Revert "Remove now-unneeded listen gem. Yay."
    This reverts commit eed350df268d5048e1f322abc6f44fd148223a11.

--- chef/cookbooks/hobbit/recipes/default.rb
+++ chef/cookbooks/hobbit/recipes/default.rb
@@ -1,4 +1,10 @@
 package "ruby-sass"
+cookbook_file "#{Chef::Config[:file_cache_path]}/listen-0.7.3.gem" do
+  source "listen-0.7.3.gem"
+end
+execute "install listen gem" do
+  command "gem install #{Chef::Config[:file_cache_path]}/listen-0.7.3.gem"
+end
 
 # TODO: Use the real postgresql providers?
 # include_recipe "database::postgresql"
