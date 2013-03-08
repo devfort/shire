9f72210fd64238feb43e70a09f32270788510491
Steve Marshall <steve@nascentguruism.com>
2013-03-08 16:26:55 +0000

    Install, configure, and futz with statsd and friends.

--- chef/cookbooks/fort/attributes/default.rb
+++ chef/cookbooks/fort/attributes/default.rb
@@ -1,1 +1,4 @@
 default[:statsd][:user] = "statsd"
+default[:statsd][:port] = 8125
+default[:statsd][:graphite][:host] = "localhost"
+default[:statsd][:graphite][:port] = 2003
--- chef/cookbooks/fort/recipes/statsd.rb
+++ chef/cookbooks/fort/recipes/statsd.rb
@@ -1,30 +1,62 @@
 package "graphite-carbon"
 
-user "statsd" do
-  home "/home/statsd"
+user node[:statsd][:user] do
+  home "/home/#{node[:statsd][:user]}"
   supports :manage_home => true
 end
 
-# TODO: Install node-syslog from npm
-if `test -d /home/#{node[:statsd][:user]}/node_modules/statsd`
-  cookbook_file "#{Chef::Config[:file_cache_path]}/statsd-0.5.0.tgz" do
-    source "statsd/statsd-0.5.0.tgz"
-    owner node[:statsd][:user]
-    group node[:statsd][:user]
-    mode 0755
+# TODO: Install from npm
+{
+  'statsd'      => "statsd-0.5.0.tgz",
+  'node-syslog' => "node-syslog-1.1.3.tar",
+}.each { |package, package_file|
+  if `test -d /home/#{node[:statsd][:user]}/node_modules/#{package}`
+    cookbook_file "#{Chef::Config[:file_cache_path]}/#{package_file}" do
+      source "statsd/#{package_file}"
+      owner node[:statsd][:user]
+      group node[:statsd][:user]
+      mode 0755
+    end
+    execute "install #{package}" do
+      command "npm install #{Chef::Config[:file_cache_path]}/#{package_file}"
+      user node[:statsd][:user]
+      cwd "/home/#{node[:statsd][:user]}"
+      environment ({"HOME" => "/home/#{node[:statsd][:user]}"})
+    end
   end
-  execute "install statsd" do
-    command "npm install #{Chef::Config[:file_cache_path]}/statsd-0.5.0.tgz"
-    user node[:statsd][:user]
-    cwd "/home/#{node[:statsd][:user]}"
-    environment ({"HOME" => "/home/#{node[:statsd][:user]}"})
-  end
+}
+
+
+directory "/home/#{node[:statsd][:user]}/etc" do
+  owner node[:statsd][:user]
+  group node[:statsd][:user]
+  mode 0755
 end
 
-cookbook_file "/etc/init/statsd.conf" do
-  source "statsd/upstart.conf"
-  notifies :restart, "service[statsd]", :immediate
+cookbook_file "/etc/default/graphite-carbon" do
+  source "statsd/graphite-carbon"
+  owner node[:statsd][:user]
+  group node[:statsd][:user]
+  mode 0644
+  notifies :restart, "service[carbon-cache]", :immediate
 end
+
+service "carbon-cache" do
+  action [:enable, :start]
+end
+
+{
+  "statsd/upstart.conf.erb" => "/etc/init/statsd.conf",
+  "statsd/config.js.erb"    => "/home/#{node[:statsd][:user]}/etc/config.js"
+}.each{ |source, target|
+  template target do
+    source source
+    owner node[:statsd][:user]
+    group node[:statsd][:user]
+    mode  0644
+    notifies :restart, "service[statsd]", :immediate
+  end
+}
 
 service "statsd" do
   provider Chef::Provider::Service::Upstart
