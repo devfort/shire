04b6be50c4680cc97059cec5a069c98585eeed55
Steve Marshall <steve@nascentguruism.com>
2013-03-08 20:02:35 +0000

    Add chef prereqs for installing pip stuff.

---
+++ chef/cookbooks/fort/metadata.rb
@@ -0,0 +1,8 @@
+name              "fort"
+maintainer        "/dev/fort"
+maintainer_email  "blah@blah.blah"
+license           "TBD"
+description       "Configures fort devenv"
+version           "0.1"
+
+depends           "python"
